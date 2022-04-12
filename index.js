0bd21c71677b684cba9b53d1a24ac7aab138ed96
Adam <adam.l.sosa@gmail.com>
2022-04-12 10:04:48 -0400

    phase-1-arithmetic-lab

--- index.js
+++ index.js
@@ -12,9 +12,16 @@
   let num2 = 31
   let multiply = num1 * num2;
 
-  Math.random(random);
+  Math.random();
+  const random = 1*2
 
+  
+  let num3 = 19;
+  let num4 = 5;
+  let mod = num3 % num4;
 
+  let max = 20;
+  Math.max(5,10,15,20);
   
 
 
